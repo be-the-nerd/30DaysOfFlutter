8ea83b7b5712241adba920748b650747a4645b03
be-the-nerd <divyanshverma010@gmail.com>
2021-11-07 20:23:27 +0530

    Day 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_catalog/home_page.dart';
+import 'package:flutter_catalog/pages/home_page.dart';
+import 'package:flutter_catalog/pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,16 +9,29 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    int days = 30;
+    /* int days = 30;
     String name = "Divyansh";
     double piValue = 3.14;
     bool isMale = true;
     num temp = 30.5;  //inherited type of the int and double types
     var day = "Tuesday";
-    const pi = 3.14;
+    const pi = 3.14; */
+
+    bringVegetables(rupees: 50, bag: true);
 
     return MaterialApp(
-      home:HomePage(),
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      themeMode: ThemeMode.light,
+      darkTheme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      initialRoute: "/home",
+      routes: {
+        "/home": (context) => LoginPage(), //same as new LoginPage()
+        "/login": (context) => LoginPage(),
+      },
     );
   }
 }
+
+bringVegetables({@required bool bag = false, int rupees = 100}) {}
