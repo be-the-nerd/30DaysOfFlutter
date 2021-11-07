8869bcef2f0e696a14263e990fa8730935a9793b
be-the-nerd <divyanshverma010@gmail.com>
2021-11-07 21:29:14 +0530

    Day 4 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/pages/home_page.dart';
 import 'package:flutter_catalog/pages/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -17,13 +18,16 @@ class MyApp extends StatelessWidget {
     var day = "Tuesday";
     const pi = 3.14; */
 
-    bringVegetables(rupees: 50, bag: true);
+    bringVegetables(bag: true, rupees: 50);
 
     return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
       themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        fontFamily: GoogleFonts.lato().fontFamily,
+      ),
       darkTheme: ThemeData(
-        primarySwatch: Colors.green,
+        brightness: Brightness.dark,
       ),
       initialRoute: "/home",
       routes: {
